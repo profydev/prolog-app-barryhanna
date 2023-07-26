86ce6dc7aba1439ac70199d558ce749002d8beeb
Barry Hanna <barryahanna@gmail.com>
2023-07-26 12:24:13 +0100

    Add footer

--- features/layout/page-container/page-container.tsx
+++ features/layout/page-container/page-container.tsx
@@ -3,6 +3,9 @@ import Head from "next/head";
 import styled from "styled-components";
 import { SidebarNavigation } from "../sidebar-navigation";
 import { color, displayFont, textFont, space, breakpoint } from "@styles/theme";
+import Link from "next/link";
+import Image from "next/image";
+import packageInfo from "../../../package.json";
 
 type PageContainerProps = {
   children: React.ReactNode;
@@ -31,6 +34,7 @@ const ContentContainer = styled.div`
   margin-top: ${({ theme }) => theme.size.headerHeight};
   padding: ${space(8, 3)};
   background: white;
+  position: relative;
 
   @media (min-width: ${breakpoint("desktop")}) {
     min-height: calc(100vh - ${space(3)} - 2 * ${space(8)});
@@ -49,7 +53,63 @@ const Title = styled.h1`
 const Info = styled.div`
   margin-bottom: ${space(8)};
   color: ${color("gray", 500)};
-  ${textFont("md", "regular")}
+  ${textFont("md", "semibold")}
+`;
+
+const Footer = styled.footer`
+  display: grid;
+  grid-template-rows: repeat(3, 1fr);
+  justify-content: center;
+  text-align: center;
+  position: absolute;
+  gap: ${space(6)};
+  padding-block: ${space(6)};
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: ${color("gray", 50)};
+  padding: 13px ${space(8)};
+
+  @media (min-width: ${breakpoint("desktop")}) {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+`;
+
+const Version = styled.div`
+  color: ${color("gray", 400)};
+  order: 2;
+
+  @media (min-width: ${breakpoint("desktop")}) {
+    order: 1;
+  }
+`;
+
+const FooterNav = styled.div`
+  display: flex;
+  gap: ${space(6)};
+  order: 0;
+
+  a {
+    color: ${color("gray", 500)};
+    text-decoration: none;
+    ${textFont("md", "medium")};
+  }
+
+  @media (min-width: ${breakpoint("desktop")}) {
+    order: 1;
+  }
+`;
+
+const Logo = styled(Image)`
+  order: 1;
+  margin-inline: auto;
+
+  @media (min-width: ${breakpoint("desktop")}) {
+    margin-inline: 0;
+    order: 2;
+  }
 `;
 
 export function PageContainer({ children, title, info }: PageContainerProps) {
@@ -70,6 +130,16 @@ export function PageContainer({ children, title, info }: PageContainerProps) {
           <Title>{title}</Title>
           <Info>{info}</Info>
           {children}
+          <Footer>
+            <Version>Version: {packageInfo.version}</Version>
+            <FooterNav>
+              <Link href="#">Docs</Link>
+              <Link href="#">API</Link>
+              <Link href="#">Help</Link>
+              <Link href="#">Community</Link>
+            </FooterNav>
+            <Logo src="/images/Logo.png" width={23} height={33} alt="" />
+          </Footer>
         </ContentContainer>
       </Main>
     </Container>
